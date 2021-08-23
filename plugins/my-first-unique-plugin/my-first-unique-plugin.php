c59b9fa0a7bf710be40045bc479a9b5a728321c4
iliatanev <itanev@devrix.com>
2021-08-23 15:17:51 +0300

    another field added into the plugin and removing the node_modules

--- plugins/my-first-unique-plugin/my-first-unique-plugin.php
+++ plugins/my-first-unique-plugin/my-first-unique-plugin.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-Plugin Name: My Test Plugin
-Description: Atruly amazing plugin
-Version: 1.0
-Author: itanev
+    Plugin Name: My Test Plugin
+    Description: Atruly amazing plugin
+    Version: 1.0
+    Author: itanev
 */
 
 class WordCountAndTimePlugin {
@@ -15,19 +15,31 @@ class WordCountAndTimePlugin {
 
     function settings() {
         add_settings_section( 'wcp_first_section', null, null, 'word-count-settings-page' );
-        add_settings_field( 'wpc_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section' );
+
+        // Location field 
+        add_settings_field( 'wpc_location', 'Display Location', array( $this, 'locationHTML' ), 'word-count-settings-page', 'wcp_first_section' );
         register_setting( 'wordcountplugin', 'wcp_location', array( 'sanitize_callback' => 'sanitize_text_field', 'default' => '0' ) );
+
+        // Field for the headline
+        add_settings_field( 'wpc_headline', 'Headline', array( $this, 'headlineHTML' ), 'word-count-settings-page', 'wcp_first_section' );
+        register_setting( 'wordcountplugin', 'wpc_headline', array( 'sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics' ) );
     }
 
+    // This is handling the second field HTML
+    function headlineHTML() { ?>
+        <input type="text" name="wpc_headline" value="<?php echo get_option( 'wpc_headline' ); ?>">
+    <?php }
+
+        // This is handling the first field HTML
     function locationHTML() { ?>
-        <select name="wcp_location" id="">
-            <option value="0">Beginning of Post</option>
-            <option value="1">End of Post</option>
+        <select name="wcp_location">
+            <option value="0" <?php selected( get_option( 'wcp_location' ), '0' ); ?>>Beginning of Post</option>
+            <option value="1" <?php selected( get_option( 'wcp_location' ), '1' ); ?>>End of Post</option>
         </select>
     <?php }
 
     function adminPage() {
-    add_options_page( 'Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array( $this, 'justHTML' ) );
+        add_options_page( 'Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array( $this, 'justHTML' ) );
     }
 
     function justHTML() { ?>
